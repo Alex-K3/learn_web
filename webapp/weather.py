0565faf404f8229bc5b0a02dc51068a0edb7cafb
Alex-K3 <skiselev94@gmail.com>
2022-06-04 15:16:42 +0300

    refactoring

--- webapp/weather.py
+++ webapp/weather.py
@@ -1,9 +1,11 @@
+from flask import current_app
 import requests
 
+
 def weather_by_city(city_name):
-    weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    weather_url = current_app.config['WEATHER_URL']
     params = {
-        "key": "3a5fce89ea704f8fb20191957222705",
+        "key": current_app.config['WEATHER_API_KEY'],
         "q": city_name,
         "format": "json",
         "num_of_days": "1",
